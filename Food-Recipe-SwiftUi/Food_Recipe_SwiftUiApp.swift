ceda3be27ae403ebff73a566c95b0d3b5f45e908
Ahmed Yamany <yamany2862016@gmail.com>
2023-07-01 12:04:14 +0300

    Initial Commit

---
+++ Food-Recipe-SwiftUi/Food_Recipe_SwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Recipe_SwiftUiApp.swift
+//  Food-Recipe-SwiftUi
+//
+//  Created by Ahmed Yamany on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Food_Recipe_SwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
